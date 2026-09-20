8392c3c3deb6494e1864d26c32b1f07a27638b3f
cashen <54cashen@gmail.com>
2026-09-20 08:13:46 +0800

    test: align information contract to v0.12.28.9

--- test/v100-information-hierarchy.test.js
+++ test/v100-information-hierarchy.test.js
@@ -9,7 +9,7 @@ const pkg = JSON.parse(await readFile(new URL("../package.json", import.meta.url
 const wrangler = await readFile(new URL("../wrangler.toml", import.meta.url), "utf8");
 
 test("v0.12.27 security hardening retains visual hierarchy baseline", () => {
-  assert.equal(pkg.version, "0.12.28.8");
+  assert.equal(pkg.version, "0.12.28.9");
   assert.match(wrangler, /APP_VERSION = "0.12.28.8"/);
 });
 
