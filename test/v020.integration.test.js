9ea4660b2b532193484fabb20ce86a88d6af4e97
cashen <54cashen@gmail.com>
2026-09-20 08:10:48 +0800

    test: run v020 integration suite through v020 gateway

--- test/v020.integration.test.js
+++ test/v020.integration.test.js
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import worker from "../src/index.js";
+import worker from "../src/v020.js";
 
 class MockKV {
   constructor() { this.map = new Map(); }
