f62b88c5fb8a131db234c20d31461cba5deb8d47
cashen <54cashen@gmail.com>
2026-09-20 08:07:08 +0800

    test: align security regression to v0.12.28.6

--- test/v128-security.test.js
+++ test/v128-security.test.js
@@ -92,7 +92,7 @@ test("production configuration disables bootstrap and enables the one-time gate"
   assert.match(wrangler, /invocation_logs = false/);
 });
 
-test("release is v0.12.28.5", async () => {
+test("release is v0.12.28.6", async () => {
   const pkg = JSON.parse(await readFile(new URL("../package.json", import.meta.url), "utf8"));
-  assert.equal(pkg.version, "0.12.28.5");
+  assert.equal(pkg.version, "0.12.28.6");
 });
