0a6ac316a4e2a6667dae0997de01956c53309693
cashen <54cashen@gmail.com>
2026-09-20 08:15:23 +0800

    release: align test/v128-security.test.js to v0.12.28.10

--- test/v128-security.test.js
+++ test/v128-security.test.js
@@ -92,7 +92,7 @@ test("production configuration disables bootstrap and enables the one-time gate"
   assert.match(wrangler, /invocation_logs = false/);
 });
 
-test("release is v0.12.28.9", async () => {
+test("release is v0.12.28.10", async () => {
   const pkg = JSON.parse(await readFile(new URL("../package.json", import.meta.url), "utf8"));
-  assert.equal(pkg.version, "0.12.28.9");
+  assert.equal(pkg.version, "0.12.28.10");
 });
