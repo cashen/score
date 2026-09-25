74dd9dad8faa41ceb5841b7d55fc18bc2800cae1
cashen <54cashen@gmail.com>
2026-09-25 09:14:39 +0800

    refactor: establish share projection domain boundary

---
+++ src/domain/share-projection.js
@@ -0,0 +1,6 @@
+/** Canonical share-domain boundary. The projection remains allow-list based. */
+import { publicProjection } from "../lib/model.js";
+
+export { publicProjection };
+
+export const SHARE_DOMAIN_VERSION = "0.13.0";
